e6a98e07ee39ba4dfd4b2bf6819d511d7af54998
Jordi Ribas <jordi.ribas@rwth-aachen.de>
2021-05-14 00:46:37 +0200

    Changed example_LLG.py to run simulations for different T and output results to a file

--- examples/example_LLG.py
+++ examples/example_LLG.py
@@ -10,8 +10,11 @@ from aiida import cmdline, engine
 from aiida.plugins import CalculationFactory
 from aiida_spirit import helpers
 from aiida_spirit.helpers import prepare_test_inputs
+from aiida.orm import Dict
+import numpy as np
 
 INPUT_DIR = path.join(path.dirname(path.realpath(__file__)), 'input_files')
+OUTPUT_DIR = path.join(path.dirname(path.realpath(__file__)), 'output_files/temperature_spins.txt')
 
 
 def test_run(spirit_code):
@@ -27,14 +30,37 @@ def test_run(spirit_code):
     inputs = prepare_test_inputs(INPUT_DIR)
     # add the spirit code to the inputs
     inputs['code'] = spirit_code
+    # prepare parameters
+    output_contents = ''
+    for i in range(0,1051,50):
+        parameters = Dict(
+            dict={
+                'llg_temperature': str(i),  # temperature noise (in K)
+                'external_field_magnitude': '0.005',  # external field of 0.005 T
+                'external_field_normal':
+                '0.0 0.0 1.0',  # external field points in z direction
+                'mu_s': '2.2',  # change spin moment to have the right size for Fe
+                'llg_n_iterations': '200000'  # limit the number of iterations
+            })
+        inputs['parameters'] = parameters
 
-    # Note: in order to submit your calculation to the aiida daemon, do:
-    # from aiida.engine import submit
-    # future = submit(CalculationFactory('spirit'), **inputs)
-    result = engine.run(CalculationFactory('spirit'), **inputs)
+        # Note: in order to submit your calculation to the aiida daemon, do:
+        # from aiida.engine import submit
+        # future = submit(CalculationFactory('spirit'), **inputs)
+        result = engine.run(CalculationFactory('spirit'), **inputs)
 
-    #computed_diff = result['spirit'].get_content()
-    print(f'Computed result: {result}')
+        #computed_diff = result['spirit'].get_content()
+        print(f'Computed result: {result}')
+
+        ret = result['retrieved']
+        with ret.open('spirit_Image-00_Spins-final.ovf') as _file:
+            spins_final = np.loadtxt(_file)
+        mag_mean = np.mean(spins_final, axis=0)
+        output_contents += str(i) + '\t' + str(mag_mean[2]) + '\n'
+
+    with open (OUTPUT_DIR, 'w') as output_file:
+        output_file.write(output_contents)
+
 
 
 @click.command()
